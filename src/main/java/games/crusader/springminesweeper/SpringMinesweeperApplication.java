ed879ff004cbb2b75231008d7c68cb08e2fe0401
Jerry Balderas <JerryBalderas@appnovation.com>
2019-04-27 10:50:15 -0500

    Initialize Project

---
+++ src/main/java/games/crusader/springminesweeper/SpringMinesweeperApplication.java
@@ -0,0 +1,13 @@
+package games.crusader.springminesweeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMinesweeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMinesweeperApplication.class, args);
+	}
+
+}
